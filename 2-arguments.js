3a5ace47610d7250fd3e4896b8e73f54f176f72d
biaj-001 <acetynne@gmail.com>
2025-05-23 17:05:50 +0300

    Update 2-arguments.js

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,3 +1,13 @@
 console.log("No argument");
 console.log("Best Argument found");
 console.log("Best School Arguments found");
+
+const numberOfArguments = process.argv.length;
+
+if(numberOfArguments===2){
+  console.log("No argument");
+}else if (numberOfArguments===3){
+  console.log("Argument found");
+}else{
+  console.log("Arguments found");
+}
