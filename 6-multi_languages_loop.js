e292d13f352a303c0e5fc22ff8036dc7aeb69c3a
biaj-001 <acetynne@gmail.com>
2025-05-24 22:35:40 +0300

    Update 6-multi_languages_loop.js

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,5 +1,8 @@
-const lines = ["C is fun" ,"Python is cool","Javascript is amazing"];
+const languages = ["C is fun" ,"Python is cool","Javascript is amazing"];
 
-for(let i=0;i<lines.length;i++){
+/*for(let i=0;i<lines.length;i++){
     console.log(lines[i]);
+}*/
+for (const lang of languages){
+    console.log(lang);
 }
