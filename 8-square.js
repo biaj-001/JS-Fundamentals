f68cfa851bc6112c6411aa64e97917b5a686224d
biaj-001 <acetynne@gmail.com>
2025-05-25 10:49:38 +0300

    Update 8-square.js

--- 8-square.js
+++ 8-square.js
@@ -1,15 +1,13 @@
-const arg1 =process.argv[2];
+const sizeStr =process.argv[2];
 
-const size = parseInt(arg1);
+const size = parseInt(sizeStr);
 
-if(isNaN(size)||size <=0 ){
+if(isNaN(size)){
     console.log("Missing size");
 }else {
+    const row ='x'.repeat(size);
+
     for(let i=0;i<size;i++){
-        let row ="" ;
-        for(let j=0;j<size;j++){
-            row += "X"
-        }
-      console.log(row);
+        console.log(row);
     }
 }
