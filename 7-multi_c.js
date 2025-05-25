c84f6f321bc2036cb11269c88c6e626d18d8f2d0
biaj-001 <acetynne@gmail.com>
2025-05-25 09:39:34 +0300

    Update 7-multi_c.js

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -2,7 +2,7 @@ const x = Number(process.argv[2]);
 
 if(isNaN(x)){
     console.log("Missing number of occurrences");
-}else if(x>0){
+}else {
     let i=0;
     while(i<x){
         console.log("C is fun");
