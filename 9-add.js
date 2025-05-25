25b33fafc92690ef5804bf7863f508fc50d1a367
biaj-001 <acetynne@gmail.com>
2025-05-25 10:24:45 +0300

    Create 9-add.js

---
+++ 9-add.js
@@ -0,0 +1,15 @@
+function add(a,b){
+    return a + b;
+}
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
+
+const num1 = parseInt(arg1);
+const num2 = parseInt(arg2);
+
+if(isNaN(num1)|| isNaN(num2)){
+    console.log("NaN");
+}else{
+    const result = add(num1,num2);
+    console.log(result);
+}
